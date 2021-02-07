520d713142fb6f48dd21084d2b35472778d7a068
petershowx <xiaoyulong1988@gmail.com>
2021-02-07 16:05:58 +0800

    Javanewfeature\pom.xml Javanewfeature\src\main\java\luozix\start\exams\caches\guava\Loadcache.md Javanewfeature\src\main\java\luozix\start\exams\concurrent\lock.md Javanewfeature\src\main\resources\logback.xml Javanewfeature\src\main\java\luozix\start\java8\action\beans\Apple.java Javanewfeature\src\main\java\luozix\start\java8\action\beans\lesson01\FisrtClass.java Javanewfeature\src\test\java\org\mvnsearch\Disrupter.webp

---
+++ Javanewfeature/src/main/java/luozix/start/java8/action/beans/Apple.java
@@ -0,0 +1,19 @@
+/**  
+* @Title: Apple.java
+* @Package luozix.start.java8.action.beans
+* @Description: TODO(用一句话描述该文件做什么)
+* @author xiaoyu xiaoyulong07@outlook.com 
+* @date 2021年2月6日 下午4:37:49
+* @version V1.0  
+*/
+package luozix.start.java8.action.beans;
+/**
+* @ClassName: Apple
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author xiaoyu xiaoyulong07@outlook.com 
+* @date 2021年2月6日 下午4:37:49
+*
+*/
+public class Apple {
+
+}
---
+++ Javanewfeature/src/main/java/luozix/start/java8/action/beans/lesson01/FisrtClass.java
@@ -0,0 +1,41 @@
+/**  
+* @Title: FisrtClass.java
+* @Package luozix.start.java8.action.beans.lesson01
+* @Description: TODO(用一句话描述该文件做什么)
+* @author xiaoyu xiaoyulong07@outlook.com 
+* @date 2021年2月6日 下午4:38:17
+* @version V1.0  
+*/
+package luozix.start.java8.action.beans.lesson01;
+/**
+* @ClassName: FisrtClass
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author xiaoyu xiaoyulong07@outlook.com 
+* @date 2021年2月6日 下午4:38:17
+*
+*/
+
+public class FisrtClass {
+
+	/**
+	 * @param args
+	* @Title: main
+	* @Description: TODO(这里用一句话描述这个方法的作用)
+	* @param @param args    设定文件
+	* @return void    返回类型
+	* @throws
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+//	Collections.sort(inventory, new Comparator<Apple>()){
+//		 public int compare(Apple a1, Apple a2) {
+//			 return a1.getWeight().compareTo(a2.getWeight());
+//		 }
+//	};
+//	
+//	inventory.sort(comparing(Apple::getWeight()));
+}
