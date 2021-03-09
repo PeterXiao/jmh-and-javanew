9e267f26b137ae03b79f41d1789f2d6e6e9f1b8a
petershowx <xiaoyulong1988@gmail.com>
2021-03-09 16:32:34 +0800

    Javanewfeature\src\main\java\luozix\start\streamxs\action\Stream01.java Javanewfeature\src\test\java\luozix\start\pattern\proxy\OrdinaryFileInvokerTest.java Javanewfeature\src\test\java\problems\StringTest.java

--- Javanewfeature/src/main/java/luozix/start/streamxs/action/Stream01.java
+++ Javanewfeature/src/main/java/luozix/start/streamxs/action/Stream01.java
@@ -1,11 +1,10 @@
-/**  
-* @Title: Stream01.java
-* @Package luozix.start.streamxs.action
-* @Description: TODO(用一句话描述该文件做什么)
-* @author xiaoyu xiaoyulong07@outlook.com 
-* @date 2021年2月10日 下午6:31:32
-* @version V1.0  
-*/
+/**
+ * @Title: Stream01.java @Package luozix.start.streamxs.action @Description: TODO(用一句话描述该文件做什么)
+ *
+ * @author xiaoyu xiaoyulong07@outlook.com
+ * @date 2021年2月10日 下午6:31:32
+ * @version V1.0
+ */
 package luozix.start.streamxs.action;
 
 import java.util.Optional;
@@ -15,43 +14,42 @@ import java.util.stream.Stream;
 import lombok.experimental.var;
 
 /**
-* @ClassName: Stream01
-* @Description: TODO(这里用一句话描述这个类的作用)
-* @author xiaoyu xiaoyulong07@outlook.com 
-* @date 2021年2月10日 下午6:31:32
-*
-*/
+ * @ClassName: Stream01 @Description: TODO(这里用一句话描述这个类的作用)
+ *
+ * @author xiaoyu xiaoyulong07@outlook.com
+ * @date 2021年2月10日 下午6:31:32
+ */
 public class Stream01 {
 
-	/**
-	 * @param args
-	* @Title: main
-	* @Description: TODO(这里用一句话描述这个方法的作用)
-	* @param @param args    设定文件
-	* @return void    返回类型
-	* @throws
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Stream.of("hello", "great").map(s -> s + " world").forEach(System.out::println);
+  /**
+   * @param args @Title: main @Description: TODO(这里用一句话描述这个方法的作用)
+   * @param @param args 设定文件
+   * @return void 返回类型
+   * @throws
+   */
+  public static void main(String[] args) {
 
-		Optional.of(new Random().nextInt(10)).filter(i -> i % 2 == 0).map(i -> "number is even:" + i)
-				.ifPresent(System.out::println);
-		var i = 3;
-		String s;
-		switch (i) {
-		case 1:
-			s = "one";
-			break;
-		case 2:
-			s = "two";
-			break;
-		case 3:
-			s = "three";
-			break;
-		default:
-			s = "unknown number";
-		}
-	}
+    // TODO Auto-generated method stub
+    Stream.of("hello", "great").map(s -> s + " world").forEach(System.out::println);
 
+    Optional.of(new Random().nextInt(10))
+        .filter(i -> i % 2 == 0)
+        .map(i -> "number is even:" + i)
+        .ifPresent(System.out::println);
+    var i = 3;
+    String s;
+    switch (i) {
+      case 1:
+        s = "one";
+        break;
+      case 2:
+        s = "two";
+        break;
+      case 3:
+        s = "three";
+        break;
+      default:
+        s = "unknown number";
+    }
+  }
 }
---
+++ Javanewfeature/src/test/java/problems/StringTest.java
@@ -0,0 +1,45 @@
+/**
+ * Project Name:java11_in_action File Name:StringTest.java Package Name:problems
+ * Date:2021年3月8日下午1:03:57 Copyright (c) 2021, xiaoyulong07@outlook.com All Rights Reserved.
+ */
+/**
+ * @Title: StringTest.java @Package problems @Description: TODO(用一句话描述该文件做什么)
+ *
+ * @author xiao xiaoyulong07@outlook.com
+ * @date 2021年3月8日 下午1:03:57
+ * @version V1.0
+ */
+package problems;
+/**
+ * ClassName:StringTest <br>
+ * Function: TODO ADD FUNCTION. <br>
+ * Reason: TODO ADD REASON. <br>
+ * Date: 2021年3月8日 下午1:03:57 <br>
+ *
+ * @author xiaoy
+ * @version
+ * @since JDK 1.8
+ * @see
+ */
+
+/**
+ * @ClassName: StringTest @Description: TODO(这里用一句话描述这个类的作用)
+ *
+ * @author xiaoyulong xiaoyulong07@outlook.com
+ * @date 2021年3月8日 下午1:03:57
+ * @version
+ * @since JDK 1.8
+ */
+public class StringTest {
+
+  public static void main(String[] args) {
+
+    System.out.println("🀎".length());
+    System.out.println("🀎".charAt(0));
+    System.out.println("🀎".charAt(1));
+
+    System.out.println("1".length());
+    System.out.println("1".charAt(0));
+    // System.out.println("1".charAt(1));  after java 9  Exception
+  }
+}
