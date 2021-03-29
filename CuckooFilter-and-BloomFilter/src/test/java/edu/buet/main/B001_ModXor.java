693c944f313218fcbd7babafb0e060aa0402979c
petershowx <xiaoyulong1988@gmail.com>
2021-03-29 10:41:26 +0800

    CuckooFilter-and-BloomFilter\pom.xml CuckooFilter-and-BloomFilter\src\test\java\edu\buet\main\B001_ModXor.java

--- CuckooFilter-and-BloomFilter/src/test/java/edu/buet/main/B001_ModXor.java
+++ CuckooFilter-and-BloomFilter/src/test/java/edu/buet/main/B001_ModXor.java
@@ -11,9 +11,12 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+
+//@Warmup(iterations = 5)
+//@Measurement(iterations = 5)
 @Fork(1)
-@Warmup(iterations = 5)
-@Measurement(iterations = 5)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
 @State(Scope.Thread)
 public class B001_ModXor {
 	private int v;
